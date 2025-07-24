ec7a324ba5c8151a6a7e9e1295f36df6aa9b004a
LucasGoulart24 <Goulart7720@gmail.com>
2025-07-24 14:07:45 -0300

    calendario

--- my-app/src/components/Calendario.vue
+++ my-app/src/components/Calendario.vue
@@ -4,6 +4,60 @@
             <h3>{{ ano }}</h3>
             <h3>{{ data }}</h3>
         </div>
+        <div class="semanas">
+            <span>{{ semanaD }}</span>
+            <span>{{ semanaS }}</span>
+            <span>{{ semanaT }}</span>
+            <span>{{ semanaQ }}</span>
+            <span>{{ semanaQ2 }}</span>
+            <span>{{ semanaS2 }}</span>
+            <span>{{ semanaS3 }}</span>
+        </div>
+        <div class="dia">
+            <span></span>
+            <span></span>
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+        </div>
+        <div class="dia">
+            <span>6</span>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+            <span>11</span>
+            <span>12</span>
+        </div>
+        <div class="dia">
+            <span>13</span>
+            <span>14</span>
+            <span>15</span>
+            <span>16</span>
+            <span>17</span>
+            <span>18</span>
+            <span>19</span>
+        </div>
+         <div class="dia">
+            <span>20</span>
+            <span>21</span>
+            <span>22</span>
+            <span>23</span>
+            <span>24</span>
+            <span>25</span>
+            <span>26</span>
+        </div>
+         <div class="dia">
+            <span>27</span>
+            <span>28</span>
+            <span>29</span>
+            <span>30</span>
+            <span>31</span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
 </template>
 
@@ -13,7 +67,14 @@ export default {
     data() {
         return {
             ano: '2025',
-            data: '24/07/2025'
+            data: '24/07/2025',
+            semanaD: 'Dom',
+            semanaS: 'Seg',
+            semanaT: 'Ter',
+            semanaQ: 'Qua',
+            semanaQ2: 'Qui',
+            semanaS2: 'Sex',
+            semanaS3: 'Sab'
         }
     }
 }
@@ -22,20 +83,53 @@ export default {
 <style>
 .Calendario{
     width: 550px;
-    padding: 5px;
+    padding: 10px;
     border-radius: 5px;
     background-color: #eee;
+    box-shadow: 0px 10px 10px 0px rgba(0,0,0,0.2);
 }
 
 .data{
     width: 100%;
+    height: 50px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+    padding: 0 45px;
 }
 
 .data h3{
     font-size: 1.5rem;
     font-weight: 400;
+}
+
+.semanas, .dia{
+    width: 100%;
+    height: 65px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+}
+
+.semanas span{
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    background-color: #ccc;
+}
+
+.dia span{
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    background-color: #ccc;
+    font-size: 1.3rem;
 }
 </style>
